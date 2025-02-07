2c01f0193b08e7c94b3ba4414535fc0f500996c0
Lauren Savrin <lsav1818@gmail.com>
2025-02-07 15:58:13 -0500

    Testing new repo sync

---
+++ CS699.R
@@ -0,0 +1,8 @@
+file<-read.csv("C:/Users/lsav1/Downloads/project_data.csv")
+dim(file)
+data<-file
+dim(data)
+sum(is.na(data)) # number of missing values in the whole dataset
+sapply(data, function(x) sum(is.na(x))) # missing values in individual columns
+df_no_missing <- data %>% drop_na()
+
